14d95742b5392473810dcad3743621cfc574993e
Hannah Melendy <hrmelendy@gmail.com>
2019-06-14 14:08:00 -0700

    add exception rule for '1' '2' and '3'

--- js/scripts.js
+++ js/scripts.js
@@ -13,24 +13,29 @@ function beepExceptions(i) {
   var inputNum = i;
   if(inputNum === 3) {
     inputNum = "I'm sorry, Dave. I'm afraid I can't do that.";
+    numArray.push(" " + inputNum);
   } else if(inputNum === 2) {
     inputNum = "Boop!";
+    numArray.push(" " + inputNum);
+    console.log(inputNum);
   } else if (inputNum === 1) {
     inputNum = "Beep!";
+    numArray.push(" " + inputNum);
   } else {
     inputNum = inputNum;
+    numArray.push(" " + inputNum);
   }
+  console.log(numArray);
 }
 
 //function to display each num individually
+var numArray = [];
 function allNum(userInput){
-  var numArray = [];
   var inputNum = 0;
   for(var i = 0; i <= userInput; i++){
     inputNum = i;
-    console.log(i);
+    // numArray.push(" " + i);
     inputNum = beepExceptions(i);
-    numArray.push(" " + inputNum);
     console.log(numArray);
   }
   numArray.forEach(function() {
