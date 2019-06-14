1da4ad0aa6f6e67f17ea004e579c39ddfc74cc2b
Hannah Melendy <hrmelendy@gmail.com>
2019-06-14 11:18:05 -0700

    add current business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
 //business logic
 
+//function to find the sum of all numbers between 0 to userInput
+function numSum(userInput){
+    var inputSum = 0;
+      for(var i = 0; i <= userInput; i++){
+        inputSum += i;
+         }
+    console.log(inputSum)
+         }
+
+//mutating numSum function to display each num in console, instead of add all num
+var userInput = parseInt(userInput);
+function numList(userInput) {
+  console.log(userInput);
+  var outputArray = [];
+  for(var i = 0; i <= userInput; i++){
+    var allNum = outputArray.push[i];
+  }
+  console.log(outputArray);
+}
+
 
 //UI logic
 
@@ -8,10 +28,10 @@ $(function() {
     event.preventDefault();
 
     var userInput = $("#numInput").val();
+    var userOutput = numSum(userInput);
 
 
-
-    $(".outputNum").text(userInput);
+    $(".outputNum").text(userOutput);
   });
 
 });
