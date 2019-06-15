f200147afbc4d4dd4500cac8b5a5b1628d0a862d
Hannah Melendy <hrmelendy@gmail.com>
2019-06-15 15:21:15 -0700

    add function to inspect individual digits in one number

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,10 @@
 //business logic
 
-// var hotKeys = ['1', '2', '3'];
-// var exceptions = ["Beep!", "Boop!", "I'm sorry, " + userName + ". I'm afraid I can't do that."];
-//
-// var output = function(userInput) {
-//   var exceptionOutput = "";
-//   for (var i = hotKeys.length-1; i >= 0; i--) {
-//     while (userInput >= hotKeys[i]) {
-//       console.log(userInput);
-//       exceptionOutput += exceptions[i];
-//       console.log(exceptionOutput);
-//       userInput -= hotKeys[i];
-//       console.log(userInput);
-//     }
-//   }
-//   return exceptionOutput;
-// }
-
-
-//adding exceptions
-function beepExceptions(i) {
-  var inputNum = i;
-  var userName = $("#nameInput").val();
-  multiDigit({userInput});
-  if(inputNum === 3) {
-    inputNum = "I'm sorry, " + userName + ". I'm afraid I can't do that.";
-    numArray.push(" " + inputNum);
-  } else if(inputNum === 2) {
-    inputNum = "Boop!";
-    numArray.push(" " + inputNum);
-  } else if (inputNum === 1) {
-    inputNum = "Beep!";
-    numArray.push(" " + inputNum);
-  } else {
-    inputNum = inputNum;
-    numArray.push(" " + inputNum);
-  }
-}
-
 // //function to check individual values in number
 // //also needs to check to see if an exception is greater than the last exception digit found
 function multiDigit(numInput) {
   var inputDigit = numInput.toString();
   var eachDigit = inputDigit.split("");
-
-  //function to display each num individually
-  var numArray = [];
-  function allNum(userInput){
-    var inputNum = 0;
-    for(var i = 0; i <= userInput; i++){
-      inputNum = i;
-      inputNum = beepExceptions(i);
-      console.log(numArray);
-    }
-    numArray.forEach(function() {
-
-    });
-    $(".outputNum").text(numArray);
-  }
 
   eachDigit.forEach(function(digit) {
     numSleuth = 0;
@@ -71,12 +18,47 @@ function multiDigit(numInput) {
     } else if ('1' === digit && 1 > numSleuth) {
       numSleuth = 1;
     }
-    alert(numSleuth);
 
   });
 
   return numSleuth;
 }
+
+
+//adding exceptions
+function beepExceptions(i) {
+  var inputNum = i;
+  var userName = $("#nameInput").val();
+  multiDigit(inputNum);
+  if(numSleuth === 3) {
+    inputNum = "I'm sorry, " + userName + ". I'm afraid I can't do that.";
+    numArray.push(" " + inputNum);
+  } else if(numSleuth === 2) {
+    inputNum = "Boop!";
+    numArray.push(" " + inputNum);
+  } else if (numSleuth === 1) {
+    inputNum = "Beep!";
+    numArray.push(" " + inputNum);
+  } else {
+    inputNum = inputNum;
+    numArray.push(" " + inputNum);
+  }
+}
+
+  //function to display each num individually
+  var numArray = [];
+  function allNum(userInput){
+    var inputNum = 0;
+    for(var i = 0; i <= userInput; i++){
+      // inputNum = i;
+      inputNum = beepExceptions(i);
+
+    }
+    numArray.forEach(function() {
+
+    });
+    $(".outputNum").text(numArray);
+  }
 
 
 
